7447da882b8e421231508f9d5a57d3aed7ba1933
Said Mounaim <saidmounaim00@gmail.com>
2020-05-31 03:46:31 +0100

    add style to card item

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -10,12 +10,12 @@ const CardItem = () => {
     return (
         <React.Fragment>
             <div className="container mx-auto">
-                <div className="grid grid-cols-4 mt-4">
+                <div className="grid md:grid-cols-4 sm:grid-cols-1 mt-4" >
                     {users && users.map((user, index) =>
-                    <div className="max-w-sm rounded relative overflow-hidden shadow-md ml-3 mb-4 pt-2 pb-2" key={index} >
-                        <img src={user.avatar_url} className="w-24 h-24 rounded-full inline-block" />
-                        <h2 className="inline-block text-1xl font-medium text-blue-900 ml-3" >{user.login}</h2>
-                        <a href="#" className="text-1xl bg-blue-500 hover:bg-blue-400 text-white font-bold p-1" >
+                    <div className="text-center max-w-sm rounded-lg relative overflow-hidden shadow-md ml-3 mb-4 pt-2 pb-2" key={index} >
+                        <img src={user.avatar_url} className="w-24 h-24 mb-3 rounded-full inline-block" />
+                        <h2 className="block mb-3 text-1xl font-medium text-blue-900 ml-3" >{user.login}</h2>
+                        <a href="#" className="block text-1xl bg-blue-500 hover:bg-blue-400 text-white font-bold p-1" >
                             View profile
                         </a>
                     </div>
